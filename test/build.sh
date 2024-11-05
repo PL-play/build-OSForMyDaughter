71a8e0e5746bb418a0c19114c021ca99fcae8f36
ran <zhangran42@126.com>
2024-11-05 23:29:10 +0800

    test

--- test/build.sh
+++ test/build.sh
@@ -1,6 +1,6 @@
 nasm -f bin -o boot.bin boot.asm
-nasm -f bin -o loader.bin loader.asm
+nasm -f bin -o loader2.bin loader2.asm
 dd if=/dev/zero of=boot.img bs=512 count=6144
 dd if=boot.bin of=boot.img bs=512 count=1 conv=notrunc
-dd if=loader.bin of=boot.img bs=512 count=5 seek=1 conv=notrunc
+dd if=loader2.bin of=boot.img bs=512 count=5 seek=1 conv=notrunc
 qemu-system-x86_64 -drive file=boot.img,format=raw
