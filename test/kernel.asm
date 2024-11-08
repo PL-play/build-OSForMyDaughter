eb24e5d6906ed09df5d9eae02a2b248aabacff9c
ran <zhangran42@126.com>
2024-11-08 15:46:25 +0800

    idt handler 0

--- test/kernel.asm
+++ test/kernel.asm
@@ -2,6 +2,18 @@
 [ORG 0x200000]
 
 start:    
+    mov rdi, idt_start          ; 将Idt 表的起始地址加载到 rdi 寄存器中，rdi 在此用作指针
+    mov rax, handler0           ; 将中断处理程序 handler0 的地址加载到 rax 中
+    ; 分几步将 handler0 地址存入 IDT 的第一个条目
+    mov [rdi], ax               ; 将 handler0 的低 16 位写入 Idt 表的第一个条目的 Offset [15:0]
+    shr rax, 16                 ; 右移 16 位，将 handler0 的中间 16 位移入低位
+    mov [rdi+6], ax             ; 将 handler0 的中间 16 位写入 Offset [31:16]
+    shr rax, 16                 ; 再次右移 16 位，以获得 handler0 的高 32 位
+    mov [rdi+8], eax            ; 将 handler0 的高 32 位写入 Offset [63:32]
+
+    lidt [idt_descriptor]       ; 加载 IDT 表
+
+
     lgdt [gdt64_descriptor]     ; 加载64位模式的GDT表指针
     push 8                      ; 将代码段选择子 8 压入栈中。这里的 8 是在GDT中定义的代码段选择子，表示64位代码段
     push kernel_entry           ; 将标签 kernel_entry 的地址压入栈中，表示要跳转到的目标地址
@@ -10,12 +22,25 @@ start:
 
 kernel_entry:
     mov rsi, message            ; 加载字符串地址到 RSI
-    mov rdi, 9                  ; 行数
+    mov rdi, 0                  ; 行数
     mov rcx, 0                  ; 列数
     call print_string           ; 调用打印函数
+
+    ; xor rbx,rbx                 ; 测试除0异常
+    ; div rbx
 end:
     hlt                         ; 停止 CPU（可根据需要调整其他代码）
     jmp end
+
+
+handler0:
+    mov rsi, divide_by_0
+    mov rdi, 0
+    mov rcx, 0
+    call print_string 
+
+    jmp end
+    iretq                   ; 返回到中断前的程序位置，这是 64 位模式下的中断返回指令
 
 print_string:
     ; 输入: RSI - 指向要打印的字符串
@@ -63,4 +88,22 @@ gdt64_descriptor:
     dq gdt64_start                     ; GDT的起始地址
 
 
-message db "Welcome to My Operating System!", 0  ; 要打印的字符串，以 0 结尾
+idt_start:
+    %rep 256            ; DT 的起始地址。使用 %rep 256 定义了 256 个条目，因为 x86 架构中 IDT 表最多包含 256 个中断向量
+        dw 0            ; 设置中断处理程序的低 16 位偏移量为 0
+        dw 0x8          ; 选择子（Selector），通常为内核代码段的段选择子
+        db 0            ; 保留字节，通常设置为 0
+        db 0x8e         ; 设置属性字段，其中 0x8e 表示这是一个中断门，具有特定的特权级和存在位
+        dw 0            ; 初始化高位偏移量为 0（即高 32 位）
+        dd 0
+        dd 0
+    %endrep
+idt_end:
+
+idt_descriptor:
+    dw idt_end-idt_start-1
+    dq idt_start
+
+
+message db     "Welcome to My Operating System!", 0  ; 要打印的字符串，以 0 结尾
+divide_by_0 db "Divided by 0                   ", 0  
