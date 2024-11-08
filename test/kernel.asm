da209f4be51dff692813b5ccd63a5fc349f9c872
ran <zhangran42@126.com>
2024-11-08 16:36:23 +0800

    save register in handler

--- test/kernel.asm
+++ test/kernel.asm
@@ -34,12 +34,49 @@ end:
 
 
 handler0:
-    mov rsi, divide_by_0
+    ; 保存上下文
+    push rax
+    push rbx
+    push rcx 
+    push rdx
+    push rsi
+    push rdi
+    push rbp
+    push r8
+    push r9
+    push r10
+    push r11 
+    push r12
+    push r13
+    push r14
+    push r15
+
+
+    mov rsi, divide_by_0        ; 打印输出
     mov rdi, 0
     mov rcx, 0
     call print_string 
 
     jmp end
+
+    ; 恢复
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rbp
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx 
+    pop rbx
+    pop rax
+
+
     iretq                   ; 返回到中断前的程序位置，这是 64 位模式下的中断返回指令
 
 print_string:
